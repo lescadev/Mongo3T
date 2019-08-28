359cdab6658135d7610143cd152259c7b6deb0ac
Mihidari <theo.lanique@gmail.com>
2019-08-28 11:13:39 +0200

    Connexion du bot

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 //Ici ça sera la logique et le corps du bot, la connexion et l'utilisation des fichiers dans /lib
+const Discord = require('discord.js');
+require('dotenv').config({path: '.env.local'});
+
+const client = new Discord.Client();
+
+client.on('ready', ()=> {
+    console.log("Ready");
+    client.user.setActivity('GitHub', {type: 'WATCHING'});
+});
+
+client.login(process.env.BOT);
